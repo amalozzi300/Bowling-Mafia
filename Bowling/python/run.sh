e34883c253b4167185412b2c50b252fcce917d61
Andrew Malozzi <95106625+amalozzi300@users.noreply.github.com>
2022-10-27 20:45:43 -0400

    Add files via upload
    add files

---
+++ Bowling/python/run.sh
@@ -0,0 +1,4 @@
+python3 scores.py < bowlers.txt
+python3 eliminator.py < elimBowlers.txt > elimStandings.txt
+python3 highGame.py < hgBowlers.txt > hgStandings.txt
+python3 doubles.py < mdTeams.txt > mdStandings.txt
