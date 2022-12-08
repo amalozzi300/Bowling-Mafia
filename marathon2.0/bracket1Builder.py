38c77b84b4e95c6528e05148f4dd3e7269397039
amalozzi300 <andrewmalozzi900@gmail.com>
2022-12-08 14:09:26 -0500

    update tournament rules

--- marathon2.0/bracket1Builder.py
+++ marathon2.0/bracket1Builder.py
@@ -1,50 +1,27 @@
 import bowler as b
+import random
+import math
 
-pairs = []
+bowlers = []
 brackets = []
 
-f = open('bracket1Pairs.txt', 'r')
-numPairs = int(f.readline())
-for i in range(numPairs):
-  temp = f.readline()
-  name1 = f.readline()
-  hdcp1 = int(f.readline())
-  name2 = f.readline()
-  hdcp2 = int(f.readline())
-  b1 = b.Bowler(name1, hdcp1)
-  b2 = b.Bowler(name2, hdcp2)
-  p = [b1, b2]
-  pairs.append(p)
+f = open('bracket1Bowlers.txt', 'r')
+numBowlers = int(f.readline())
+for i in range(numBowlers):
+  name = f.readline()
+  hdcp = int(f.readline())
+  b1 = b.Bowler(name, hdcp)
+  bowlers.append(b1)
 f.close()
 
-print(pairs[0][0])
-print(pairs[0][1])
+numBracks = int(numBowlers / 8)
+if numBowlers & 8 != 0:
+  numBracks += 1
 
-brackets.append([pairs[0][0], pairs[0][1]])
-
-for i in range(1, len(pairs)):
-  p = pairs[i]
-  added = False
-  
-  for j in range(len(brackets)):
-    b = brackets[j]
-    inB = False
-
-    if not added and len(b) != 8:
-      for k in range(len(b)):
-        if p[0] == b[k] or p[1] == b[k]:
-          inB = True
-      
-      if not inB:
-        b.append(p[0])
-        b.append(p[1])
-        added = True
-
-  if not added:
-    brackets.append([p[0], p[1]])
 
 
 f = open('bracket1.txt', 'w')
+f.write(str(numBracks))
 for i in range(len(brackets)):
   for j in range(len(brackets[i])):
     f.write(str(brackets[i][j]) + '\n')
